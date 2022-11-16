37433b9e84bd63e52e803fd2b8e1228abad584ab
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2022-11-15 17:07:04 -0800

    added assertion using Chai

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,6 +1,9 @@
 const { Builder, By, Key } = require("selenium-webdriver");
 const assert = require("assert");
 
+// no need to add .assert() or .expect(), only if want to use should()
+var should = require("chai").should();
+
 async function exampleTest() {
   // launch the browser
   let driver = await new Builder().forBrowser("chrome").build();
@@ -21,8 +24,11 @@ async function exampleTest() {
     .then(function (value) {
       return value;
     });
+  // assert using node assertion
+  // assert.strictEqual(todoText, "Learn Selenium");
 
-  assert.strictEqual(todoText, "Learn Selenium");
+  // assert using chai should
+  todoText.should.equal("Learn Selenium");
 
   // close the borwser
   await driver.quit();
